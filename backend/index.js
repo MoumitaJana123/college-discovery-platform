975ba566603243446c5fdc7251be7cf70af5c7a6
MoumitaJana123 <moumita2003jana@gmail.com>
2026-05-09 17:21:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,17 @@ app.use(cors());
 app.use(express.json());
 
 // Database Connection
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+// });
+// Database Connection
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  // This version works whether NODE_ENV is set or not
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 //  SEARCH/GET allL Colleges
